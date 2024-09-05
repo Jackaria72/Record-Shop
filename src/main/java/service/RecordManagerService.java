4216972a6465c8df9815b708da296b7760525f41
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-05 11:31:47 +0100

    Feat: impliment method to get all records

--- src/main/java/service/RecordManagerService.java
+++ src/main/java/service/RecordManagerService.java
@@ -1,4 +1,10 @@
 package service;
 
+import model.RecordModel;
+
+import java.util.List;
+
 public interface RecordManagerService {
+
+    List<RecordModel> getAllRecords();
 }
--- src/main/java/service/RecordManagerServiceImpl.java
+++ src/main/java/service/RecordManagerServiceImpl.java
@@ -1,8 +1,12 @@
 package service;
 
+import model.RecordModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.RecordManagerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RecordManagerServiceImpl implements RecordManagerService {
@@ -10,4 +14,10 @@ public class RecordManagerServiceImpl implements RecordManagerService {
     @Autowired
     RecordManagerRepository recordManagerRepository;
 
+    @Override
+    public List<RecordModel> getAllRecords() {
+        List<RecordModel> allRecords = new ArrayList<>();
+        recordManagerRepository.findAll().forEach(allRecords::add);
+        return allRecords;
+    }
 }
