55d79741cd28bc26c92752f3715752a0134a1f18
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-05 15:29:09 +0100

    Test: pass test for find by id in repository layer

--- src/test/java/com/northcoders/record_shop/repository/RecordManagerRepositoryTest.java
+++ src/test/java/com/northcoders/record_shop/repository/RecordManagerRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -26,5 +28,15 @@ class RecordManagerRepositoryTest {
 
         // Assert
         assertThat(records).hasSize(1);
+    }
+
+    @Test
+    public void testFindRecordByIdReturnsRecord() {
+        RecordModel record = new RecordModel(1L, "Vol.3: The Subliminal Verses", "Slipknot", "nuMetal", 2004, 3);
+        recordManagerRepository.save(record);
+
+        RecordModel records = recordManagerRepository.findById(1L).get();
+
+        assertThat(records).isEqualTo(record);
     }
 }
