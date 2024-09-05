87f5b424f9bd8659cd33b479b027ec42f73d19c4
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-05 12:20:23 +0100

    Feat: impliment get all records to controller layer

--- src/main/java/com/northcoders/record_shop/controllers/RecordManagerController.java
+++ src/main/java/com/northcoders/record_shop/controllers/RecordManagerController.java
@@ -1,9 +1,15 @@
 package com.northcoders.record_shop.controllers;
 
+import com.northcoders.record_shop.model.RecordModel;
 import com.northcoders.record_shop.service.RecordManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/record")
@@ -11,5 +17,11 @@ public class RecordManagerController {
 
     @Autowired
     RecordManagerService recordManagerService;
+
+    @GetMapping("/all-records")
+    public ResponseEntity<List<RecordModel>> getAllRecords() {
+        List<RecordModel> records = recordManagerService.getAllRecords();
+        return new ResponseEntity<>(records, HttpStatus.OK);
+    }
 
 }
