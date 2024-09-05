e755836ee430161fe9a0f888e74741ca69d0d265
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-05 16:19:37 +0100

    Test: pass test for find by id in service layer

--- src/test/java/com/northcoders/record_shop/service/RecordManagerServiceImplTest.java
+++ src/test/java/com/northcoders/record_shop/service/RecordManagerServiceImplTest.java
@@ -10,6 +10,7 @@ import com.northcoders.record_shop.repository.RecordManagerRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -38,6 +39,24 @@ class RecordManagerServiceImplTest {
 
         assertThat(expectedResult).hasSize(4);
         assertThat(expectedResult).isEqualTo(recordList);
+
+    }
+
+    @Test
+    void testGetByIdGetsBook() {
+
+        Long id1 = 1L;
+
+        RecordModel test1 = new RecordModel(1L, "Vol.3: The Subliminal Verses", "Slipknot", "nuMetal", 2004, 3);
+//        recordList.add(new RecordModel(2L, "Follow the Leader", "Korn", "nuMetal", 1998, 2));
+//        recordList.add(new RecordModel(3L, "Nevermind", "Nirvana", "Grunge", 1991, 35));
+//        recordList.add(new RecordModel(4L, "Enema Of The State", "blink-182", "pop-punk", 1999, 1));
+
+        when(mockRecordManagerRepository.findById(id1)).thenReturn(Optional.of(test1));
+
+        RecordModel result = recordManagerServiceImpl.getRecordById(id1);
+
+        assertThat(result).isEqualTo(test1);
 
     }
 }
