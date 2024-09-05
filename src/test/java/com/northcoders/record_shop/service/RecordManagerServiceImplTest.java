994729415f5f6f0e95881b086bea78af08dea8b7
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-05 14:48:26 +0100

    Refactor: refactor for record data in get all test

--- src/test/java/com/northcoders/record_shop/service/RecordManagerServiceImplTest.java
+++ src/test/java/com/northcoders/record_shop/service/RecordManagerServiceImplTest.java
@@ -30,7 +30,7 @@ class RecordManagerServiceImplTest {
         recordList.add(new RecordModel(1L, "Vol.3: The Subliminal Verses", "Slipknot", "nuMetal", 2004, 3));
         recordList.add(new RecordModel(2L, "Follow the Leader", "Korn", "nuMetal", 1998, 2));
         recordList.add(new RecordModel(3L, "Nevermind", "Nirvana", "Grunge", 1991, 35));
-        recordList.add(new RecordModel(1L, "Enema Of The State", "blink-182", "pop-punk", 1999, 1));
+        recordList.add(new RecordModel(4L, "Enema Of The State", "blink-182", "pop-punk", 1999, 1));
 
         when(mockRecordManagerRepository.findAll()).thenReturn(recordList);
 
