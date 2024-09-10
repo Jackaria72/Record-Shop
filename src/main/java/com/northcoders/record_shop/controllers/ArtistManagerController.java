968c224a6837135f7788219a8c2e8641618fe849
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-10 23:20:34 +0100

    Feat: implemented get artist by id in controller layer

--- src/main/java/com/northcoders/record_shop/controllers/ArtistManagerController.java
+++ src/main/java/com/northcoders/record_shop/controllers/ArtistManagerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class ArtistManagerController {
     public ResponseEntity<List<Artist>> getAllArtists() {
         List<Artist> artists = artistManagerService.getAllArtists();
         return new ResponseEntity<>(artists, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Artist> getArtistById(@PathVariable Long id) {
+        Artist artist = artistManagerService.getArtistById(id);
+        return new ResponseEntity<>(artist, HttpStatus.OK);
     }
 }
