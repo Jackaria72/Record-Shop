3479d6fa12907c69f750503b1f873e08f11f78ab
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-10 23:14:03 +0100

    Feat: implemented get all artist controller layer

--- src/main/java/com/northcoders/record_shop/controllers/ArtistManagerController.java
+++ src/main/java/com/northcoders/record_shop/controllers/ArtistManagerController.java
@@ -1,9 +1,16 @@
 package com.northcoders.record_shop.controllers;
 
+import com.northcoders.record_shop.model.Artist;
 import com.northcoders.record_shop.service.ArtistManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/artist")
@@ -12,5 +19,9 @@ public class ArtistManagerController {
     @Autowired
     ArtistManagerService artistManagerService;
 
-
+    @GetMapping("/all-artists")
+    public ResponseEntity<List<Artist>> getAllArtists() {
+        List<Artist> artists = artistManagerService.getAllArtists();
+        return new ResponseEntity<>(artists, HttpStatus.OK);
+    }
 }
