e8b597f22c48d6abf52e7a229fe2be578eaaa83d
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-11 12:10:48 +0100

    Feat: add CRUD query for finding all albums with stock greater than a given amount

--- src/main/java/com/northcoders/record_shop/repository/RecordManagerRepository.java
+++ src/main/java/com/northcoders/record_shop/repository/RecordManagerRepository.java
@@ -4,6 +4,10 @@ import com.northcoders.record_shop.model.Album;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordManagerRepository extends CrudRepository<Album, Long> {
+    List<Album> findByQuantityInStockGreaterThan(int quantityInStock);
+
 }
