da5d4e43a68627d0f407774cde1d54793045dbec
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-09-11 19:46:53 +0100

    Feat: add CRUD query to find artists by name

--- src/main/java/com/northcoders/record_shop/repository/ArtistManagerRepository.java
+++ src/main/java/com/northcoders/record_shop/repository/ArtistManagerRepository.java
@@ -4,6 +4,10 @@ import com.northcoders.record_shop.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface ArtistManagerRepository extends CrudRepository<Artist, Long> {
+    Optional<Artist> findByArtistName(String artistName);
 }
